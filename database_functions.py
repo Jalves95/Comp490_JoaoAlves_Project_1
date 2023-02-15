a91851972ec72a9ba89a0d48295dad5359ee9e7c
jalves95 <j7alves@student.bridgew.edu>
2023-02-14 21:49:42 -0500

    removed getting the data from the data_base file into it's proper getData file. Easier to read the program

--- database_functions.py
+++ database_functions.py
@@ -1,37 +1,11 @@
 import sqlite3
-import requests
-import secrets
-from requests.auth import HTTPBasicAuth
-
-
-base_url = "https://joaoalves.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
+from getData import safe_get_request, save_data
 
 
 def print_red_text(text_str: str):
     """ Prints the incoming string parameter as RED text to the terminal """
 
     print(f'\033[91m{text_str}\033[00m')
-
-
-def safe_get_request() -> dict:
-    """ This function makes a Get request to the URL passed in as its single parameter.
-    If an exception is thrown while trying to execute the GET request, 'None' is returned
-    in place of a response object. If the GET request is successful, a response object is returned. """
-
-    try:
-        # if request.get() throws an exception, the 'response' variable will remain as 'None'
-        response = requests.get(base_url, auth=HTTPBasicAuth(secrets.API_FOR_GET_REQUEST, 'pass'))
-        if response.status_code != 200:  # if we don't get an ok response we have trouble
-            print_red_text(f"Failed to get data, response code:{response.status_code} and error message:"
-                           f" {response.reason} ")
-        print(f'GET request executed with no errors. Response object created:\n'
-              f'Response object: <{hex(id(response))}>\n')
-    except requests.exceptions.RequestException as requests_exception:
-        print_red_text(f'GET requests FAILED with the following error: {requests_exception}\n')
-    finally:
-        print(f'The Get request was successful \n{response.status_code}[{response.reason}]\n')
-        json_response = response.json()
-        return json_response
 
 
 def establish_database_connection(database_name: str):
@@ -88,17 +62,6 @@ def create_db_connection():
     return db_connection
 
 
-def save_data(data_to_save: list, save_file=None):
-    """ Provided by Dr. Santore's sprint 1 solution"""
-
-    for entry in data_to_save:
-        for key, value in entry.items():
-            print(f"{key}: {value}", file=save_file)
-        # now print the spacer
-        print("+++++++++++++++++++++++++++++++++++++++++++++\n_______________________________________________",
-              file=save_file)
-
-
 def create_tables(cursor: sqlite3.Cursor, dictionary: dict):
     """ This function creates each tables from the wufoo Database. """
 
@@ -118,7 +81,7 @@ def create_table_columns(cursor: sqlite3.Cursor, wufoo_data):
                          f'First_Name TEXT,' \
                          f'Last_Name TEXT,' \
                          f'Title TEXT,' \
-                         f'rganization_Name TEXT,' \
+                         f'Organization_Name TEXT,' \
                          f'Email TEXT,' \
                          f'Organization_Website TEXT,' \
                          f'Phone_Number TEXT,' \
@@ -146,7 +109,6 @@ def create_wufoo_db():
     file_to_save = open("data_output.txt", 'w')
     save_data(data1, save_file=file_to_save)
     print(data1)
-
 
     try:
         # Creates database connection
---
+++ getData.py
@@ -0,0 +1,43 @@
+import requests
+import secrets
+from requests.auth import HTTPBasicAuth
+
+base_url = "https://joaoalves.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
+
+
+def print_red_text(text_str: str):
+    """ Prints the incoming string parameter as RED text to the terminal """
+
+    print(f'\033[91m{text_str}\033[00m')
+
+
+def safe_get_request() -> dict:
+    """ This function makes a Get request to the URL passed in as its single parameter.
+    If an exception is thrown while trying to execute the GET request, 'None' is returned
+    in place of a response object. If the GET request is successful, a response object is returned. """
+
+    try:
+        # if request.get() throws an exception, the 'response' variable will remain as 'None'
+        response = requests.get(base_url, auth=HTTPBasicAuth(secrets.API_FOR_GET_REQUEST, 'pass'))
+        if response.status_code != 200:  # if we don't get an ok response we have trouble
+            print_red_text(f"Failed to get data, response code:{response.status_code} and error message:"
+                           f" {response.reason} ")
+        print(f'GET request executed with no errors. Response object created:\n'
+              f'Response object: <{hex(id(response))}>\n')
+    except requests.exceptions.RequestException as requests_exception:
+        print_red_text(f'GET requests FAILED with the following error: {requests_exception}\n')
+    finally:
+        print(f'The Get request was successful \n{response.status_code}[{response.reason}]\n')
+        json_response = response.json()
+        return json_response
+
+
+def save_data(data_to_save: list, save_file=None):
+    """ Provided by Dr. Santore's sprint 1 solution"""
+
+    for entry in data_to_save:
+        for key, value in entry.items():
+            print(f"{key}: {value}", file=save_file)
+        # now print the spacer
+        print("+++++++++++++++++++++++++++++++++++++++++++++\n_______________________________________________",
+              file=save_file)
