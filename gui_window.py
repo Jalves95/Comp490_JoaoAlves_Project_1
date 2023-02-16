c05b9ea7b75ab8ce60308ad6669d9d8ab72b7d72
jalves95 <j7alves@student.bridgew.edu>
2023-02-15 21:32:41 -0500

    Check boxes input

--- gui_window.py
+++ gui_window.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QListWidget, QLineEdit, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QListWidget, QLineEdit, QLabel, QCheckBox
 from PySide6.QtGui import QCloseEvent
 
 
@@ -18,7 +18,7 @@ class GuiWindow(QWidget):
         btn_quit = QPushButton('Quit', self)
         btn_quit.clicked.connect(QApplication.instance().quit)
         btn_quit.resize(btn_quit.sizeHint())
-        btn_quit.move(600, 650)
+        btn_quit.move(630, 650)
 
         display_list = QListWidget(self)
         self.list_control = display_list
@@ -39,20 +39,62 @@ class GuiWindow(QWidget):
         title_display = QLineEdit(self)
         title_display.move(375, 100)
 
+        label = QLabel("Phone_Number:", self)
+        label.move(310, 150)
+        title_display = QLineEdit(self)
+        title_display.move(400, 150)
+
         label = QLabel("Title:", self)
         label.move(520, 0)
         org_display = QLineEdit(self)
         org_display.move(600, 0)
 
-        label = QLabel("Organization:", self)
+        label = QLabel("Org_Name:", self)
         label.move(520, 50)
         email_display = QLineEdit(self)
         email_display.move(600, 50)
 
-        label = QLabel("Email:", self)
+        label = QLabel("Org_Website:", self)
         label.move(520, 100)
         last_name_display = QLineEdit(self)
         last_name_display.move(600, 100)
+
+        label = QLabel("Email:", self)
+        label.move(550, 150)
+        last_name_display = QLineEdit(self)
+        last_name_display.move(600, 150)
+
+        checkbox = QCheckBox("Course_Project", self)
+        checkbox.move(310, 200)
+
+        checkbox = QCheckBox("Guest Speaker", self)
+        checkbox.move(310, 230)
+
+        checkbox = QCheckBox("Site Visit", self)
+        checkbox.move(310, 260)
+
+        checkbox = QCheckBox("Job Shadow", self)
+        checkbox.move(310, 290)
+
+        checkbox = QCheckBox("Internship", self)
+        checkbox.move(310, 320)
+
+        checkbox = QCheckBox("Career Panel", self)
+        checkbox.move(310, 350)
+
+        checkbox = QCheckBox("Networking Event", self)
+        checkbox.move(310, 380)
+
+        checkbox = QCheckBox("Summer 2022", self)
+        checkbox.move(520, 200)
+        checkbox = QCheckBox("Fall 2022", self)
+        checkbox.move(520, 230)
+        checkbox = QCheckBox("Spring 2023", self)
+        checkbox.move(520, 260)
+        checkbox = QCheckBox("Summer 2023", self)
+        checkbox.move(520, 290)
+        checkbox = QCheckBox("Other", self)
+        checkbox.move(520, 320)
 
         self.show()
 
